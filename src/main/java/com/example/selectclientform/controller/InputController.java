655734d4c0fad04eef549424b0f46050289df6b2
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2019-07-16 15:09:53 +0900

    add coments

--- src/main/java/com/example/selectclientform/controller/InputController.java
+++ src/main/java/com/example/selectclientform/controller/InputController.java
@@ -3,6 +3,10 @@ package com.example.selectclientform.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ * 画面表示のみを行っています。
+ * Ajaxとは無関係です。
+ */
 @Controller
 public class InputController {
 
--- src/main/java/com/example/selectclientform/controller/SelecterController.java
+++ src/main/java/com/example/selectclientform/controller/SelecterController.java
@@ -13,12 +13,20 @@ import com.example.selectclientform.dto.ClientDto;
 import com.example.selectclientform.mapper.ClientMapper;
 import com.google.gson.Gson;
 
+/*
+ * ajaxを使った非同期のリクエストを受け取るRestControllerクラスです。
+ * http通信でhtmlファイル以外のデータを扱う場合はRestControllerを使います。
+ */
 @RestController
 public class SelecterController {
 
 	@Autowired
 	ClientMapper mapper;
 
+	/*
+	 * DBから取得した値をjson形式にして返却します。
+	 * https://ja.wikipedia.org/wiki/JavaScript_Object_Notation
+	 */
 	@RequestMapping("selecter")
 	public String showCreateFrom(@RequestParam(name = "client", required = false) String client) {
 
@@ -26,6 +34,7 @@ public class SelecterController {
 		map.put("client", client);
 		List<ClientDto> list = mapper.getSelectList(map);
 
+		// Gsonというライブラリを使ってJavaのオブジェクトをjson形式に変換します。
 		Gson gson = new Gson();
 		return gson.toJson(list);
 	}
